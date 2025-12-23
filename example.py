a82be1caf490c11109b872d3e0358c1548ba9bef
ろく <ririkasatou1207@gmail.com>
2025-12-23 17:16:54 +0900

    python

--- example.py
+++ example.py
@@ -1,1 +1,17 @@
 print("hello world")
+
+def greet():
+    return "こんにちは"
+print(greet())
+
+def print_name(name):
+    return f"私の名前は{name}です"
+print(print_name("ゴジラ"))
+
+def get_greet():
+    return "おはようございます"
+print(get_greet())
+
+def add(a, b):
+    return a + b
+print(add(2, 4))
